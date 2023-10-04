8a833ae370ecf640f465153e96689078f771e02e
taccat3 <tcaplan172@gmail.com>
2023-10-04 15:21:48 -0400

    base files added

---
+++ server.improved.js
@@ -0,0 +1,19 @@
+const http = require( 'http' ),
+      fs   = require( 'fs' ),
+      express = require('express'),
+      dotenv = require("dotenv"),
+      app = express()
+
+// allows use of environment variables
+dotenv.config()
+
+// use express.urlencoded to get data sent by default form actions
+// or GET requests
+app.use( express.urlencoded({ extended: true }) )
+
+// public directory
+app.use(express.static('./public'))
+app.use(express.json())
+
+// set up the server
+app.listen(`${process.env.PORT}`)
