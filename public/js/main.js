304d64a9b697980a518a55655e23e2260df463a8
taccat3 <tcaplan172@gmail.com>
2023-10-09 21:00:22 -0400

    basic features implemented - singular blade option

--- public/js/main.js
+++ public/js/main.js
@@ -26,16 +26,44 @@ var engine
 var render
 
 words = {}
-bodies = []
+projectiles = []
 
 // global variables
 mousePressed = false
 slicing = false
 
+// blade variables
+let bladeCanvas
+let bladeCTX
+let isDown=false
+let startX
+let startY
+let offsetX
+let offsetY
+
+currentBlade = null
+
+function Blade(down, up, move) {
+    this.down = down
+    this.up = up
+    this.move = move
+    this.enable = () => {
+        document.getElementById('matter-js-canvas').addEventListener('mousedown', down)
+        document.getElementById('matter-js-canvas').addEventListener('mouseup', up)
+        document.getElementById('matter-js-canvas').addEventListener('mousemove', move)
+    }
+    this.disable = () => {
+        this.up()
+        document.getElementById('matter-js-canvas').removeEventListener('mousedown', down)
+        document.getElementById('matter-js-canvas').removeEventListener('mouseup', up)
+        document.getElementById('matter-js-canvas').removeEventListener('mousemove', move)
+    }
+}
+
 sliced = event => {
     if(mousePressed === true && slicing === false) {
         slicing = true
-        bodiesAtPos = Matter.Query.point(bodies, event.mouse.position);
+        bodiesAtPos = Matter.Query.point(projectiles, event.mouse.position);
 
         bodiesAtPos.forEach(body => {
             if(body.isStatic === false && lives > 0) {
@@ -47,6 +75,7 @@ sliced = event => {
                 } else if (body.label === 'bomb') {
                     body.parts[0].render.sprite.texture = generateWordImage('bomb', SLICED_COLOR)
                     lives = 0
+                    currentBlade.disable()
                     document.getElementById('lives').innerHTML = lives
                 } else {
                         score++
@@ -67,13 +96,14 @@ collisionDetected = event => {
 
         // check if was collision with the floor
         if(pair.bodyA.label === 'the floor' || pair.bodyB.label === 'the floor') {
-            projectile = pair.bodyA.label === 'the floor' ? pair.bodyB : pairBodyA
+            projectile = pair.bodyA.label === 'the floor' ? pair.bodyB : pair.bodyA
             // check if a bomb collided with the floor
-            if(projectile.label !== 'bomb' && Body.getVelocity(projectile).y < 0) {
+            if(projectile.label !== 'bomb' && Body.getVelocity(projectile).y > 0 && lives > 0) {
                 lives--
                 document.getElementById('lives').innerHTML = lives
             }
             World.remove(engine.world, projectile)
+            projectiles.filter(p => p.label !== projectile.label)
         }
     }
 }
@@ -132,12 +162,13 @@ function getNumberOfWords(text) {
 
 function generateWordImage(word, color) {
     var imageCanvas = document.createElement('canvas')
-    imageCanvas.width = word.length * 15
-    imageCanvas.height = 30
     var ctx2 = imageCanvas.getContext('2d')
     ctx2.font = "30px Arial"
+    imageCanvas.width = ctx2.measureText(word).width;
+    imageCanvas.height = 30
+    ctx2.font = "30px Arial"
     ctx2.fillStyle = color
-    ctx2.fillText(word, 10, 25, 150)
+    ctx2.fillText(word, 0, 25, 150)
 
     return imageCanvas.toDataURL('image/png')
 }
@@ -261,11 +292,61 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function resizeCanvases() {
+    WIDTH = window.innerWidth - MARGIN
+    HEIGHT = window.innerHeight - MARGIN
+    render.canvas.setAttribute('width', WIDTH + "px")
+    render.canvas.setAttribute('height', HEIGHT + "px")
+    bladeCanvas.setAttribute('width', WIDTH + "px")
+    bladeCanvas.setAttribute('height', HEIGHT + "px")
+    bladeCTX = bladeCanvas.getContext('2d')
+    offsetX=bladeCanvas.offsetLeft
+    offsetY=bladeCanvas.offsetTop
+}
+
+function getCurrentBlade() {
+    mouseDownEvent = event => {
+        event.preventDefault();
+        startX=parseInt(event.clientX-offsetX);
+        startY=parseInt(event.clientY-offsetY);
+        isDown=true;
+    }
+
+    mouseUpEvent = (event=null) => {
+        if(event) {
+            event.preventDefault()
+        }
+        isDown = false
+        bladeCTX.clearRect(0, 0, bladeCanvas.width, bladeCanvas.height);    
+    }
+
+    mouseMoveEvent = event => {
+        if(!isDown){return;}
+        event.preventDefault();
+        
+        mouseX=parseInt(event.clientX-offsetX);
+        mouseY=parseInt(event.clientY-offsetY);
+        
+        bladeCTX.beginPath();  // use beginPath for every segment of the line
+        bladeCTX.moveTo(startX,startY);
+        bladeCTX.lineTo(mouseX,mouseY);
+        bladeCTX.fillStyle = 'black'
+        bladeCTX.stroke();
+        startX=mouseX;
+        startY=mouseY;
+    }
+
+    return new Blade(mouseDownEvent, mouseUpEvent, mouseMoveEvent)
+}
+
 window.onload = async () => {
 
     document.getElementById('counter').innerHTML = score
     document.getElementById('lives').innerHTML = lives
     document.getElementById('money').innerHTML = money
+
+    bladeCanvas = document.getElementById('canvas-blade')
+    bladeCTX = bladeCanvas.getContext('2d')
 
     // get the word list from the server
     await fetch('words').then(response => {
@@ -294,7 +375,7 @@ window.onload = async () => {
         
         // create a renderer
         render = Render.create({
-            element: document.getElementById('canvas'),
+            element: document.getElementById('canvases'),
             engine: engine,
             showCollisions: true,
             options: {
@@ -302,15 +383,13 @@ window.onload = async () => {
                 wireframes: false,
                 width: window.innerWidth - MARGIN,
                 height: window.innerHeight - MARGIN,
-                background: 'yellow'
+                background: 'transparent'
             },
         });
 
         // set canvas size
-        WIDTH = window.innerWidth - MARGIN
-        HEIGHT = window.innerHeight - MARGIN
-        render.canvas.setAttribute('width', WIDTH + "px")
-        render.canvas.setAttribute('height', HEIGHT + "px")
+        resizeCanvases()
+        
 
         // add mouse control
         var mouse = Mouse.create(render.canvas)
@@ -341,10 +420,7 @@ window.onload = async () => {
         
         //  resize canvas if window resizes
         addEventListener('resize', (event) => {
-            WIDTH = window.innerWidth - MARGIN
-            HEIGHT = window.innerHeight - MARGIN
-            render.canvas.setAttribute('width', WIDTH + "px")
-            render.canvas.setAttribute('height', HEIGHT + "px")
+            resizeCanvases()
             World.remove(engine.world, floor)
             floor = getFloor()
             World.add(engine.world, floor)
@@ -358,6 +434,10 @@ window.onload = async () => {
         // run the renderer
         Render.run(render);
 
+        // enable current blade
+        currentBlade = getCurrentBlade()
+        currentBlade.enable()
+
         // start the game
         update()  
     })
@@ -370,28 +450,31 @@ update = async function() {
         waittime = Math.random() * 3000 + 1000
         await delay(waittime).then()
         
-        // temporal recursion, call the function in the future
-        window.requestAnimationFrame( update )
-    
-        switch (Math.floor(Math.random() * 10)) {
-            case 0: // normal word
-                body = getRandomWordProjectile()
-                bodies.push(body);
-                break;
-            case 1: // money word
-                body = getMoneyProjectile()
-                bodies.push(body);
-                break;
-            case 2: // bomb
-                body = getBombProjectile()
-                bodies.push(body);
-                break;
-            default: // normal word
-                body = getRandomWordProjectile()
-                bodies.push(body);
-                break;
+        // check again after delay
+        if(lives > 0) {
+            // temporal recursion, call the function in the future
+            window.requestAnimationFrame( update )
+                
+            switch (Math.floor(Math.random() * 10)) {
+                case 0: // normal word
+                    body = getRandomWordProjectile()
+                    projectiles.push(body);
+                    break;
+                case 1: // money word
+                    body = getMoneyProjectile()
+                    projectiles.push(body);
+                    break;
+                case 2: // bomb
+                    body = getBombProjectile()
+                    projectiles.push(body);
+                    break;
+                default: // normal word
+                    body = getRandomWordProjectile()
+                    projectiles.push(body);
+                    break;
+            }
         }
-        
+       
         World.add(engine.world, body)
     }
 }
